f0199a855f34703d3bac1739da61deb3cba670c6
Nayana M R <nayana.m.r@gds.ey.com>
2024-02-28 01:28:12 +0530

    9066 added state management lib files

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export const sharedStore = configureStore({
   reducer: counterReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof sharedStore.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof sharedStore.dispatch;
